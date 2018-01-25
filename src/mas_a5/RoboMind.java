998fe413f7c0d3ad1afa77a35554f24ec1647abb
Graca <s4magrac@uni-trier.de>
2018-01-25 14:12:21 +0100

    Angepasste Q Berechnung

--- src/mas_a5/RoboMind.java
+++ src/mas_a5/RoboMind.java
@@ -1,5 +1,7 @@
 package mas_a5;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import mas_a5.Koordinaten.Koordinate;
@@ -10,13 +12,13 @@ public class RoboMind {
 
 	// Rewardliste
 	private ConcurrentHashMap<String, Integer> rewList;
-	
+
 	// Qliste
-	private ConcurrentHashMap<String, Double> qList;	
-	
+	private ConcurrentHashMap<String, Double> qList;
+
 	// Arrived check
 	private int arrived;
-	
+
 	private RoboMind() {
 		this.rewList = new ConcurrentHashMap<String, Integer>();
 		this.qList = new ConcurrentHashMap<String, Double>();
@@ -26,70 +28,120 @@ public class RoboMind {
 	public static RoboMind getInstance() {
 		if (instance == null) {
 			instance = new RoboMind();
-		} 
+		}
 		return instance;
 	}
 
 	public void addReward(String key, int r) {
 		rewList.put(key, r);
 	}
-	
+
 	public void addReward(Koordinate k) {
 		rewList.put(k.toString(), k.getReward());
 	}
-	
+
 	public int getReward(String key) {
 		return rewList.get(key);
 	}
-	
+
 	public void addQVal(String key, double d) {
 		qList.put(key, d);
 	}
-	
+
 	public double getQVal(String key) {
 		return qList.get(key);
 	}
-	
+
 	public void arrived() {
 		arrived++;
 	}
-	
+
 	public boolean allBotsArrived() {
 		return arrived == 2;
 	}
-	
+
 	public void resetArrived() {
 		arrived = 0;
 	}
-	
-	private void updateQList(int actionX, int actionY, int currentX, int currentY){
-		int options = 0;
-		if (rewList.containsKey((currentX - 1) + "_" + currentY)) options++;
-		if (rewList.containsKey((currentX + 1) + "_" + currentY)) options++;
-		if (rewList.containsKey(currentX + "_" + (currentY-1))) options++;
-		if (rewList.containsKey(currentX + "_" + (currentY+1))) options++;		
-				
-		double qValue = 0.0;
-		double rewardAction = getReward(actionX + "_" + actionY);
-		double p = 1/options;
-		double maxQValueAction = 0.0;
-		double qValueAction1 = 0.0;
-		double qValueAction2 = 0.0;
-		double qValueAction3 = 0.0;
-		double qValueAction4 = 0.0;
-		double x;
+
+	// Q(state, action) = R(state, action) + Gamma * Max[Q(next state, all
+	// actions)]
+	private void updateQList(int currentX, int currentY) {
+
+		double rewardCurrState = getReward(currentX + "_" + currentY);
+
+		double p = 1 / optionsCount(currentX, currentY);
+
+		HashMap<String, Double> qValues = new HashMap<String, Double>();
+
+		String keyRight = (currentX + 1) + "_" + currentY;
+		String keyLeft = (currentX - 1) + "_" + currentY;
+		String keyUp = currentX + "_" + (currentY + 1);
+		String keyDown = currentX + "_" + (currentY - 1);
 		
-		qValueAction1 = rewardAction + (p * getQVal((actionX + 1) + "_" + actionY));
-		qValueAction2 = rewardAction + (p * getQVal((actionX - 1) + "_" + actionY));
-		qValueAction3 = rewardAction + (p * getQVal(actionX + "_" + (actionY+1)));
-		qValueAction4 = rewardAction + (p * getQVal(actionX + "_" + (actionY-1)));
+		// Reward rechts
+		if (rewList.containsKey(keyRight)) {
+			double qValueAction1 = rewardCurrState + (p * getQVal(keyRight));
+			qValues.put(keyRight, qValueAction1);
+		}
+
+		// Reward links
+		if (rewList.containsKey(keyLeft)) {
+			double qValueAction2 = rewardCurrState + (p * getQVal(keyLeft));
+			qValues.put(keyLeft, qValueAction2);
+		}
+
+		// Reward oben
+		if (rewList.containsKey(keyUp)) {
+			double qValueAction3 = rewardCurrState + (p * getQVal(keyUp));
+			qValues.put(keyUp, qValueAction3);
+		}
+
+		// Reward unten
+		if (rewList.containsKey(keyDown)) {
+			double qValueAction4 = rewardCurrState + (p * getQVal(keyDown));
+			qValues.put(keyDown, qValueAction4);
+		}
+
+		// Max der einzelnen Actions
+		Map.Entry<String, Double> maxEntry = null;
+
+		for (Map.Entry<String, Double> entry : qValues.entrySet())
+		{
+		    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+		    {
+		        maxEntry = entry;
+		    }
+		}
 		
+		double oldQVal = getQVal(maxEntry.getKey());
+		double newQVal = maxEntry.getValue() + oldQVal;
 		
-		qValue = Math.max(qValueAction1, qValueAction2);
-		qValue = Math.max(qValue, qValueAction3);
-		qValue = Math.max(qValue, qValueAction4);
+		addQVal(maxEntry.getKey(), newQVal);
 		
-		//QValue = Reward bei Acttion
-				
+		// QValue = Reward bei Acttion
+
+	}
+
+	private int optionsCount(int currentX, int currentY) {
+		int cnt = 0;
+		// Rechts
+		if (rewList.containsKey((currentX + 1) + "_" + currentY))
+			;
+		cnt++;
+		// Links
+		if (rewList.containsKey((currentX + 1) + "_" + currentY))
+			;
+		cnt++;
+		// Oben
+		if (rewList.containsKey((currentX + 1) + "_" + currentY))
+			;
+		cnt++;
+		// Unten
+		if (rewList.containsKey((currentX + 1) + "_" + currentY))
+			;
+		cnt++;
+
+		return cnt;
 	}
 }
